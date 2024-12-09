b45d9f14e65f5a9f33039dc26a026950862d2675
Sabin Bhattarai <95851999+codenothappening@users.noreply.github.com>
2024-12-09 16:34:27 +0545

    First Comment

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/configs/theme/app_color.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColor.primary,
+    scaffoldBackgroundColor: AppColor.lightBackground,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColor.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColor.primary,
+    scaffoldBackgroundColor: AppColor.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColor.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
